33ed61cc8291a6cb6624a7328db66550be5a18cf
Tope Afolabi <afolabitope@ymail.com>
2020-10-26 14:20:34 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from "history";
+import store from './redux/store';
 import './index.css';
-import App from './App';
+import './assets/css/index.output.css';
+// import App from './App';
+import Landing from './ui/pages/Landing'
+import Faq from './ui/pages/Faq'
 import reportWebVitals from './reportWebVitals';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router history={ history }>
+      <Route path="/" exact={true} component={Landing}></Route>
+      <Route path="/faq" exact={true} component={Faq}></Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
